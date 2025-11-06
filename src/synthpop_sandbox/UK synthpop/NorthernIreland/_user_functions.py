f67fce83b73248035ffb47ede86a44a38ccefa8b
paddy-r <prehr@leeds.ac.uk>
2025-11-06 12:58:46 +0000

    Retrieve and merge NI boundaries and pop centroids

--- src/synthpop_sandbox/UK synthpop/NorthernIreland/_user_functions.py
+++ src/synthpop_sandbox/UK synthpop/NorthernIreland/_user_functions.py
@@ -8,8 +8,9 @@
 ### Imports ###
 import os
 from os.path import dirname as up
+import pandas as pd
 import geopandas as gpd
-
+from torch.utils.collect_env import get_os
 
 ### Definitions ###
 NI_PATH = up(__file__)
@@ -25,8 +26,8 @@ COMPASS_PATH = os.path.join(HOME_PATH, 'data', 'compass')  # HR 30/10/25 Updated
 SCOTLAND_PATH = os.path.join(HOME_PATH, 'data', '_From UK synthpop', 'Scotland')  # HR 31/10/25 Microdata and US from Scotland synthpop
 
 # Spatial stuff
-GEOJSON_NI = ''
-POP_CENTROIDS_NI = ''
+GEOJSON_NI = os.path.join(PERSISTENT_DATA_PATH, 'DZ2021.geojson')
+POP_CENTROIDS_NI = os.path.join(PERSISTENT_DATA_PATH, 'census-2021-population-weighted-centroids-data-zone.csv')
 
 
 ### Utility functions ###
@@ -34,15 +35,27 @@ print('Importing utility functions...')
 
 
 # HR 06/11/25 Grab NI spatial boundaries for mapping/visualisation
-def get_spatial_boundaries_ni():
-    boundaries = ''
+def get_spatial_boundaries_ni(file_fullpath=GEOJSON_NI):
+    boundaries = gpd.read_file(file_fullpath)
     return boundaries
 
-def get_population_centroids_ni():
-    centroids = ''
+def get_population_centroids_ni(file_fullpath=POP_CENTROIDS_NI):
+    centroids = pd.read_csv(file_fullpath)
     return centroids
+
+def merge_boundaries_centroids_ni(boundaries=None, centroids=None):
+    if boundaries is None:
+        boundaries = get_spatial_boundaries_ni()
+    if centroids is None:
+        centroids = get_population_centroids_ni()
+
+    centroids.rename(columns={'DZ2021_code': 'DZ2021_cd', 'DZ2021_name': 'DZ2021_nm', 'X': 'centroid_x', 'Y': 'centroid_y'}, inplace=True)
+    centroids.drop(columns=['DZ2021_nm'], inplace=True)
+
+    merge_key = 'DZ2021_cd'
+    merged = boundaries.merge(centroids, on=merge_key)
+    return merged
 
 
 if __name__ == "__main__":
-    b = get_spatial_boundaries_ni()
-    c = get_population_centroids_ni()
+    m = merge_boundaries_centroids_ni()  # Testing retrieval and merging of all spatial data
